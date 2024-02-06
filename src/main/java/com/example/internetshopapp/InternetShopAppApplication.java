88e94cf2ecf90628d6a21c1fedb82918d6401c97
pai-puella <cat.dyshkant@yandex.ru>
2024-02-06 13:21:18 +0300

    initial commit

---
+++ src/main/java/com/example/internetshopapp/InternetShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.internetshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopAppApplication.class, args);
+	}
+
+}
